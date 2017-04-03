668e99b11e99acc1efd0c9bc4b62cf6a62e4918c
Aleksandr Korovin <Aleksandr Korovin>
2017-04-03 13:21:56 +0200

    basic structure

---
+++ module/module.ts
@@ -0,0 +1,11 @@
+/**
+ * Created by korovin on 4/3/2017.
+ */
+namespace Module {
+    export function example(msg) {
+        let a = 1;
+        return {
+            lalala: 123
+        }
+    }
+}
